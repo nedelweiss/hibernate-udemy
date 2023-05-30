7d7b8b3af67155e28f0d7a09c4b67eb08a335e0c
nedelweiss <edelweissnull@gmail.com>
2023-05-30 17:06:02 +0300

    Initial commit

---
+++ src/main/java/com/github/nedelweiss/hibernateudemy/HibernateUdemyApplication.java
@@ -0,0 +1,13 @@
+package com.github.nedelweiss.hibernateudemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateUdemyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HibernateUdemyApplication.class, args);
+    }
+
+}
